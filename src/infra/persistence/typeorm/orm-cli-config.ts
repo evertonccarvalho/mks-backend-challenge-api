e871676a1929ca18bb0552066e7f795da1c52b21
Éverton Carvalho <evertonsnake@gmail.com>
2024-05-16 01:19:16 -0300

    refactor: delete movies mvc

--- src/infra/persistence/typeorm/orm-cli-config.ts
+++ src/infra/persistence/typeorm/orm-cli-config.ts
@@ -1,5 +1,6 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { MovieEntity } from './entities/movie.entity';
+import { UserEntity } from '@/users/entities/user.entity';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
@@ -8,7 +9,7 @@ export const dataSourceOptions: DataSourceOptions = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [MovieEntity],
+  entities: [MovieEntity, UserEntity],
   synchronize: false,
 };
 
@@ -17,20 +18,3 @@ export const dataSource = new DataSource({
   synchronize: false,
   migrations: [],
 });
-
-// import { MovieRepository } from '@/application/mskmovies/ports/movie.repositoy';
-// import { Module } from '@nestjs/common';
-// import { TyperOrmMoviesRepository } from './repositories/typeorm-movies.repository';
-// import { PostgresModule } from './PostGresModule';
-
-// @Module({
-//   imports: [PostgresModule],
-//   providers: [
-//     {
-//       provide: MovieRepository,
-//       useClass: TyperOrmMoviesRepository,
-//     },
-//   ],
-//   exports: [MovieRepository],
-// })
-// export class DatabaseModule {}
